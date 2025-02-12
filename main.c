dcccf3a8c2b65b86eb5a7f93ad63af8b12779a35
Anunstu <anun.lue@student.mahidol.edu>
2025-02-12 08:58:09 +0000

    updated again

--- main.c
+++ main.c
@@ -25,6 +25,10 @@ int main(int argc , char **argv) {
             {
               printf("dequeing %d\n",x);
             }
+            else
+            {
+              q.size=0;
+            }
         }
         else {
        enqueue_struct(&q, atoi(argv[i]));
